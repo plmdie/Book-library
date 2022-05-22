85629c9e7b489c2703a1e49945595e7cbeffb1ac
plmdie <piratasdoarenitoexplosiv0@protonmail.com>
2022-05-22 19:13:48 +0200

    Finished app logic

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
-const modal = document.getElementById("modal");
-const addBook = document.getElementById("add-book");
-const addNewBook = document.getElementById("addBtn");
+window.onload = () => {
+    addEvents();
+    showBooks();
+}
 
 //move to function
 const bookTitleEl = document.getElementById("book-title");
@@ -10,16 +11,37 @@ const pagesEl = document.getElementById("pages");
 const statusEl = document.querySelector("#read");
 const cards = document.querySelector("#cards");
 
-let library = [];
+let library = [
+    {
+        title: 'Dune',
+        author: 'jean lol',
+        pages: 198,
+        status: true     
+    },
 
-//create function start event listeners
+    {
+        title: 'Dune II',
+        author: 'jean pierre',
+        pages: 154,
+        status: false     
+    }
+];
 
 
-addBook.onclick = () => modal.style.display = "block";
+//change how function works by reading event target
+function addEvents() {
+    const modal = document.getElementById("modal");
+    const addBook = document.getElementById("add-book");
+    const addNewBook = document.getElementById("add-new-book");
 
-addNewBook.onclick = () => addToLibrary();
+    addBook.onclick = () => modal.style.display = "block";
+    addNewBook.onsubmit = function(e) {
+        e.preventDefault();
+        addToLibrary();    
+    } 
 
-window.onclick = (event) => {if (event.target == modal) modal.style.display = "none";}
+    window.onclick = (event) => {if (event.target == modal) modal.style.display = "none";}
+}
 
 
 class Book {
@@ -39,9 +61,12 @@ class Book {
 function addToLibrary() {
     const [title, author, pages, status] = getBookData();
     const newBook = new Book(title, author, pages, status);
-    library.push(newBook);
-    modal.style.display = "none";
-    updateBookList();
+    const validate = library.find(book => book.title === title);
+    if (!validate) {
+        library.push(newBook);
+        modal.style.display = "none";
+        updateBookList();
+    } else alert("Book already exists in library");
 }
 
 function getBookData() {
@@ -54,9 +79,11 @@ function getBookData() {
 
 function showBooks() {
     library.forEach((el, index) => {
-        createBookCard(el.title, el.author, el.pages, el.status, index);
+        const valid = document.querySelector(`.card${index}`);
+        if (!valid) createBookCard(el.title, el.author, el.pages, el.status, index);
     }); 
 
+    
 }
 
 function createBookCard(title, author, pages, status, index) {
@@ -65,7 +92,6 @@ function createBookCard(title, author, pages, status, index) {
     const cardItem = document.createElement('div');
     cardItem.classList.add(`card`);
     cardItem.classList.add(`card${index}`);
-    cardItem.setAttribute('data-title', index);
     cardItem.innerHTML = `
         <p>${title}</p>
         <p>${author}</p>
@@ -77,14 +103,21 @@ function createBookCard(title, author, pages, status, index) {
     
     //create read status button
     const readStatus = document.createElement('button');
+    readStatus.setAttribute('data-title', index);
+    readStatus.classList.add(`change-status`);
     readStatus.classList.add(`${status}`); 
     status ? readStatus.textContent = 'Finished' : readStatus.textContent = 'Not finished';
     lastCard.appendChild(readStatus);   
 
     //create remove book button
     const removeBtn = document.createElement('button');
+    removeBtn.classList.add("remove-button");
+    removeBtn.setAttribute('data-title', index);
     removeBtn.textContent = 'Remove book';
     lastCard.appendChild(removeBtn);
+    
+    //add event listener
+    updateEventListener();
 }
 
 function updateBookList() {
@@ -92,14 +125,41 @@ function updateBookList() {
     createBookCard(library[i].title, library[i].author, library[i].pages, library[i].status, i); 
 }
 
-function removeBook() {
+function removeBook(e) {
+    const bookItem = e.target.getAttribute("data-title");
+    const element = document.querySelector(`.card${bookItem}`);
 
+    library.splice(bookItem, 1);
+    element.remove();
 }
 
-function changeStatus() {
+function changeStatus(e) {
+    const bookItem = e.target.getAttribute("data-title");
 
+    if (library[bookItem].status) {
+        e.target.classList.replace("true", "false");
+        e.target.textContent = "Not finished";
+    } else {
+        e.target.classList.replace("false", "true");
+        e.target.textContent = "Finished";
+    }
+    
+    library[bookItem].getStatus();
 }
+
 
 function clearLibrary() {
+    for (let i = 0; i < library.length; i++) {
+        document.querySelector(`.card${i}`).remove();    
+        
+    }
+    
+}
 
+function updateEventListener() {
+    const statusCheckbox = document.querySelectorAll(".change-status");
+    const removeButtons = document.querySelectorAll(".remove-button");
+
+    statusCheckbox.forEach(item => item.addEventListener('click', changeStatus));
+    removeButtons.forEach(item => item.addEventListener('click', removeBook));
 }
